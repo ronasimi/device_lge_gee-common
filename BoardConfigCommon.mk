27a91b9013ea0e1fb628dc873bc2b044a2c50f0d
Ron Asimi <ron.asimi@gmail.com>
2014-05-02 10:45:11 -0400

    remove cmhw, init, and references in boardconfig

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -155,10 +155,6 @@ HAVE_ADRENO_SOURCE:= false
 
 -include vendor/lge/gee/BoardConfigVendor.mk
 
-TARGET_INIT_VENDOR_LIB := libinit_msm
-
-BOARD_HARDWARE_CLASS := device/lge/gee-common/cmhw/
-
 #TWRP config
 DEVICE_RESOLUTION := 720x1280
 RECOVERY_SDCARD_ON_DATA := true
