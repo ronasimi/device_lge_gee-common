f43a92ce0b61f4793b5f7d8b0bd74b50544a10b8
Ron Asimi <ron.asimi@gmail.com>
2014-05-05 22:01:47 -0400

    add libinit_msm back

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -159,6 +159,8 @@ HAVE_ADRENO_SOURCE:= false
 
 -include vendor/lge/gee/BoardConfigVendor.mk
 
+TARGET_INIT_VENDOR_LIB := libinit_msm
+
 BOARD_HARDWARE_CLASS := device/lge/gee-common/cmhw/
 
 #TWRP config
