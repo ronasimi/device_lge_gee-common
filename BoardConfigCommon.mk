ea7e41900e649552a915323bf51816038e332ed5
Ron Asimi <ron.asimi@gmail.com>
2014-05-12 19:29:22 -0400

    jeebus H. powerhal

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -71,8 +71,6 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/lge/gee-common/bluetooth
 
 # Use the CM PowerHAL
 TARGET_USES_CM_POWERHAL := true
-CM_POWERHAL_EXTENSION := qcom
-TARGET_POWERHAL_NO_TOUCH_BOOST := true
 
 # FIXME: HOSTAPD-derived wifi driver
 BOARD_HAS_QCOM_WLAN := true
